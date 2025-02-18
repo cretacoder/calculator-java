7342b45a363cf2b250236d726f354a1ec6f00b6a
cretacoder <ajaytherani24@gmail.com>
2025-02-18 17:44:23 +0530

    Added square root

--- Calculator.java
+++ Calculator.java
@@ -134,6 +134,11 @@ public class Calculator {
                                 displayLabel.setText(displayLabel.getText() + buttonValue);
                             }
                         }
+                        else if (buttonValue == "√"){
+                            double numDisplay = Double.parseDouble(displayLabel.getText());
+                            numDisplay = Math.sqrt(numDisplay);
+                            displayLabel.setText(removeZeroDecimal(numDisplay));
+                        }
                         else if ("0123456789".contains(buttonValue)) {
                             if (displayLabel.getText() == "0") {
                                 displayLabel.setText(buttonValue);
